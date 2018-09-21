349de50e83b53f5f54d580f2972035a10fa0bff9
Angela M. Herring <angelah@lanl.gov>
2018-09-21 13:45:07 -0600

    skeleton pr scripts

---
+++ README.py
@@ -0,0 +1,14 @@
+# Example builds
+
+Below we list copy & paste instructions for several local machines; we
+have a script that parses this README file to execute the examples
+below to ensure they build.
+
+## Varan
+
+Execute the following from the wonton root directory:
+
+```c++
+# machine=varan
+echo "this is a test on varan"
+```
--- jenkins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "This should be running our tests!"
