dfa2a2c6e919527bb61e6b900b0f26a690c386d3
Rao Garimella <rao@lanl.gov>
2019-03-05 14:53:45 -0700

    eliminate space in assignment statement for thrust_flags

--- jenkins/build_matrix_entry.sh
+++ jenkins/build_matrix_entry.sh
@@ -67,7 +67,7 @@ elif [[ $build_type == "serial" ]]; then
     mpi_flags=
     jali_flags=         # jali is not available in serial
 elif [[ $build_type == "thrust" ]]; then
-    thrust_flags = "-D ENABLE_THRUST=True -DTHRUST_DIR:FILEPATH=${thrust_dir}"
+    thrust_flags="-D ENABLE_THRUST=True -DTHRUST_DIR:FILEPATH=${thrust_dir}"
 fi
 
 export SHELL=/bin/sh
