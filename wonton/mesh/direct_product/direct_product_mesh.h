8ec668fe246de1a3b75b6857e6f955fe47e1c8e8
Brendan K. Krueger <bkkrueger@lanl.gov>
2019-03-21 16:13:43 -0600

    Add direct_product_mesh_wrapper
    Added appropriate stuff to direct_product_mesh, some tweaks, some notes about
    things to fix later on.

--- wonton/mesh/direct_product/direct_product_mesh.h
+++ wonton/mesh/direct_product/direct_product_mesh.h
@@ -8,10 +8,8 @@ Please see the license file at the root of this repository, or at:
 #define WONTON_DIRECT_PRODUCT_MESH_H_
 
 #include <vector>
-#include <memory>
 
 #include "wonton/support/wonton.h"
-#include "wonton/support/Point.h"
 
 /*!
   @file direct_product_mesh.h
@@ -23,6 +21,8 @@ Please see the license file at the root of this repository, or at:
  */
 
 namespace Wonton {
+
+    // TODO: Should I switch the dimensionality to use templating?
 
 /*!
   @class Direct_Product_Mesh "direct_product_mesh.h"
@@ -40,7 +40,7 @@ namespace Wonton {
   of the tests expands, the scope of functionality of the Direct_Product_mesh
   may also expand.
  */
-class Simple_Mesh {
+class Direct_Product_Mesh {
 
  public:
 
@@ -86,6 +86,28 @@ class Simple_Mesh {
   ~Direct_Product_Mesh();
 
 
+  // ==========================================================================
+  // Accessors
+
+  // TODO: doxygen
+  // TODO: move definition outside of class, but leave declaration here
+  int get_dimensionality() const {
+      return dimensionality_;
+  }
+
+  // TODO: doxygen
+  // TODO: move definition outside of class, but leave declaration here
+  int axis_num_points(const int dim) const {
+    return edges_[dim].size();
+  }
+
+  // TODO: doxygen
+  // TODO: move definition outside of class, but leave declaration here
+  double axis_point_coordinate(const int dim, const int pointid) const {
+    return edges_[dim][pointid];
+  }
+
+
  private:
 
   // ==========================================================================
@@ -108,23 +130,26 @@ class Simple_Mesh {
   // ==========================================================================
   // Class data
 
+  //! Maximum dimensionality allowed
+  static const int MAXDIM_ = 3;
+
   //! Dimensionality of the mesh
   int dimensionality_ = -1;
 
   //! Cell edge coordinates along the three axes
-  std::vector<double> edges_i_, edges_j_, edges_k_;
+  std::vector<double>[MAXDIM_] edges_;
 
 };  // class Direct_Product_Mesh
+
 
 // ============================================================================
 // Constructors and destructors
 
 // ____________________________________________________________________________
 // 1D constructor (see class definition for declaration).
-Direct_Product_Mesh::Direct_Product_mesh(
-    const std::vector<double> edges) :
-    edges_i_(edges) {
+Direct_Product_Mesh::Direct_Product_mesh(const std::vector<double> edges) {
   dimensionality_ = 1;
+  edges_[0] = edges;
   set_default_coordinates();
 }
 
@@ -132,14 +157,14 @@ Direct_Product_Mesh::Direct_Product_mesh(
 // 2D constructor (see class definition for declaration).
 Direct_Product_Mesh::Direct_Product_mesh(
     const std::vector<double> edges_i,
-    const std::vector<double> edges_j) :
-    edges_i_(edges_i),
-    edges_j_(edges_j) {
+    const std::vector<double> edges_j) {
+  dimensionality_ = 2;
   // TODO: Note that this may have to be modified.  For example, in 2D you can
   // have cylindrical coordinates (s,z) or polar coordinates (s, phi), and
   // fitting those both into the standard 3D cylindrical (s, phi, z) requires
-  // that 2D cylindrical will assign edges_j to edges_k_ not edges_j_.
-  dimensionality_ = 2;
+  // that 2D cylindrical will assign edges_j to edges_[2] not edges_[1].
+  edges_[0] = edges_i;
+  edges_[1] = edges_j;
   set_default_coordinates();
 }
 
@@ -148,11 +173,11 @@ Direct_Product_Mesh::Direct_Product_mesh(
 Direct_Product_Mesh::Direct_Product_mesh(
     const std::vector<double> edges_i,
     const std::vector<double> edges_j,
-    const std::vector<double> edges_k) :
-    edges_i_(edges_i),
-    edges_j_(edges_j),
-    edges_j_(edges_k) {
+    const std::vector<double> edges_k) {
   dimensionality_ = 3;
+  edges_[0] = edges_i;
+  edges_[1] = edges_j;
+  edges_[2] = edges_k;
   set_default_coordinates();
 }
 
@@ -160,9 +185,9 @@ Direct_Product_Mesh::Direct_Product_mesh(
 // destructor (see class definition for declaration).
 Direct_Product_Mesh::~Direct_Product_mesh() {
   dimensionality_ = -1;
-  edges_i_.clear();
-  edges_j_.clear();
-  edges_k_.clear();
+  for (d = 0, d < MAXDIM_; ++d) {
+    edges_[d].clear();
+  }
 }
 
 
@@ -172,12 +197,12 @@ void set_default_coordinates() {
   switch(dimensionality_) {
     case 1 :
       // Cartesian coordinates (currently no others available)
-      edges_j_ = { 0.0, 1.0 };
-      edges_k_ = { 0.0, 1.0 };
+      edges_[1] = { 0.0, 1.0 };
+      edges_[2] = { 0.0, 1.0 };
       break;
     case 2 :
       // Cartesian coordinates (currently no others available)
-      edges_k_ = { 0.0, 1.0 };
+      edges_[2] = { 0.0, 1.0 };
       break;
     case 3 :
       // All edge coordinate arrays are specified, so do nothing.  This clause
