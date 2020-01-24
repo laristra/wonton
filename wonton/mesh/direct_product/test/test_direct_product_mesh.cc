03d76bb0bbb7f77583ed901c0d75424f028bad4f
Rao Garimella <rao@abyzou.lanl.gov>
2020-01-24 13:21:01 -0700

    expand to support parallel tests

--- wonton/mesh/direct_product/test/test_direct_product_mesh.cc
+++ wonton/mesh/direct_product/test/test_direct_product_mesh.cc
@@ -16,101 +16,400 @@ Please see the license file at the root of this repository, or at:
 
 #include "gtest/gtest.h"
 
+#ifdef WONTON_ENABLE_MPI
+
+using Wonton::Entity_type::PARALLEL_OWNED;
+using Wonton::Entity_type::PARALLEL_GHOST;
+using Wonton::Entity_type::ALL;
+
 // ============================================================================
 /*!
   @file test_direct_product_mesh.cc
   @brief Tests for the direct_product mesh definition in direct_product_mesh.h
+
 */
-TEST(Direct_Product_Mesh, SingleCell3D) {
-
-  const int D = 3;
-
-  // Create a single cell mesh
-  const std::vector<double> axis_points1 = {0.0, 1.0};
-  std::array<std::vector<double>,D> axis_points;
-  for (int d = 0; d < D; ++d) {
-    axis_points[d] = axis_points1;
-  }
-  Wonton::Direct_Product_Mesh<D> mesh(axis_points);
-
-  // Dimensionality
-  ASSERT_EQ(mesh.space_dimension(), D);
-
-  // Cell counts
-  for (int d = 0; d < D; ++d) {
-    ASSERT_EQ(mesh.num_axis_points(d), (int) axis_points1.size());
-  }
-
-  // Cell coordinates
-  for (int d = 0; d < D; ++d) {
-    for (int n = 0; n < mesh.num_axis_points(d); ++n) {
-      ASSERT_EQ(mesh.get_axis_point(d,n), 0.0 + 1.0*n);
-    }
-  }
-
-}
-
 
 // ============================================================================
 
-TEST(Direct_Product_Mesh, SmallGrid2D) {
+TEST(Direct_Product_Mesh, Parallel1D) {
+  double Inf = std::numeric_limits<double>::infinity();
 
-  const int D = 2;
+  bool distributed = false;
+  
+  Wonton::Executor_type *executor;
+#ifdef WONTON_ENABLE_MPI
+  Wonton::MPIExecutor_type parallel_executor(MPI_COMM_WORLD);
+  executor = &parallel_executor;
 
-  // Create a simple 2x2 mesh
-  const std::vector<double> axis_points1 = {0.0, 0.5, 1.0};
-  std::array<std::vector<double>,D> axis_points;
-  for (int d = 0; d < D; ++d) {
-    axis_points[d] = axis_points1;
-  }
-  Wonton::Direct_Product_Mesh<D> mesh(axis_points);
-
-  // Dimensionality
-  ASSERT_EQ(mesh.space_dimension(), D);
-
-  // Cell counts
-  for (int d = 0; d < D; ++d) {
-    ASSERT_EQ(mesh.num_axis_points(d), (int) axis_points1.size());
-  }
-
-  // Cell coordinates
-  for (int d = 0; d < D; ++d) {
-    for (int n = 0; n < mesh.num_axis_points(d); ++n) {
-      ASSERT_EQ(mesh.get_axis_point(d,n), 0.0 + 0.5*n);
-    }
-  }
-
-}
-
-
-// ============================================================================
-
-TEST(Direct_Product_Mesh, SmallGrid1D) {
+  // Parallel checks will fail if we are not running this on 4 ranks
+  int nprocs;
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  ASSERT_TRUE(nprocs == 1 || nprocs == 4);
+  if (nprocs > 1) distributed = true;
+#else
+  Wonton::SerialExecutor_type serial_executor;
+  executor = &serial_executor;
+#endif
 
   const int D = 1;
 
-  // Create a single cell mesh
-  //                           2^-4    2^-3   2^-2  2^-1 2^0
+  // Create a 4 cell mesh with uneven spacing
+  //                                        2^-4    2^-3   2^-2  2^-1 2^0
   const std::vector<double> axis_points1 = {0.0625, 0.125, 0.25, 0.5, 1.0};
   std::array<std::vector<double>,D> axis_points;
-  for (int d = 0; d < D; ++d) {
+  for (int d = 0; d < D; ++d)
     axis_points[d] = axis_points1;
-  }
-  Wonton::Direct_Product_Mesh<D> mesh(axis_points);
+  Wonton::Direct_Product_Mesh<D> mesh(axis_points, executor);
 
   // Dimensionality
   ASSERT_EQ(mesh.space_dimension(), D);
 
-  // Cell counts
-  ASSERT_EQ(mesh.num_axis_points(0), (int) axis_points1.size());
+  // Point counts along each axis (since points shared by processors
+  // are owned on some and ghost on others, we cannot rely on separate
+  // owned, ghost point counts)
 
-  // Cell coordinates
+  int NPOINT_ALL = distributed ? 4 : 5;
+    
+  // Default query - ALL points
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NPOINT_ALL, mesh.axis_num_points(d));
+
+  // ALL cells queried explicitly
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NPOINT_ALL, mesh.axis_num_points(d, ALL));
+
+  
+  // Cell counts along each axis
+  int NCELL_OWNED = distributed ? 1 : 4;
+  int NCELL_GHOST = distributed ? 2 : 0;
+  int NCELL_ALL = NCELL_OWNED + NCELL_GHOST;
+  
+  // Default query - ALL
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELL_ALL, mesh.axis_num_cells(d));
+
+  // All cells queried explicitly
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELL_ALL, mesh.axis_num_cells(d, ALL));
+
+  // Owned cell counts
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELL_OWNED, mesh.axis_num_cells(d, PARALLEL_OWNED));
+
+  // Ghost cell counts
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELL_GHOST, mesh.axis_num_cells(d, PARALLEL_GHOST));
+
+  // Verify we are correctly identifying exterior points
   for (int d = 0; d < D; ++d) {
-    for (int n = 0; n < mesh.num_axis_points(d); ++n) {
-      ASSERT_EQ(mesh.get_axis_point(d,n), std::pow(0.5,4-n));
+    double lo, hi;
+    mesh.get_global_coord_bounds(d, &lo, &hi);
+
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n; // ghost point starts at -1 not 0
+      double coord = mesh.get_axis_point(d, id);
+      if (fabs(coord) == Inf || coord == lo || coord == hi)
+        ASSERT_TRUE(mesh.point_on_external_boundary(d, id));
+      else
+        ASSERT_FALSE(mesh.point_on_external_boundary(d, id));
     }
   }
+  
+  // Cell coordinates (owned coordinate index starts from 0, ghost from -1)
+  for (int d = 0; d < D; ++d) {
+    double plo, phi;
+    mesh.get_local_coord_bounds(d, &plo, &phi);
 
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n;
+      double expval1, expval2;
+      
+      if (n == 0) {
+        expval1 = plo;
+        expval2 = plo;
+      } else if (n == npall-1) {
+        expval1 = phi;
+        expval2 = phi;
+      } else {
+        expval1 = (plo != -Inf) ? pow(2,n)*plo : phi/pow(2,npall-n-1);
+        expval2 = (phi !=  Inf) ? phi/pow(2,npall-n-1) : pow(2,n)*plo;
+      }
+        
+      double coord = mesh.get_axis_point(d, id);
+      ASSERT_EQ(expval1, coord);
+      ASSERT_EQ(expval2, coord);
+    }
+  }
 }
 
 
+TEST(Direct_Product_Mesh, Parallel2D) {
+  double Inf = std::numeric_limits<double>::infinity();
+
+  bool distributed = false;
+  
+  Wonton::Executor_type *executor;
+#ifdef WONTON_ENABLE_MPI
+  Wonton::MPIExecutor_type parallel_executor(MPI_COMM_WORLD);
+  executor = &parallel_executor;
+
+  // Parallel checks will fail if we are not running this on 4 ranks
+  int nprocs;
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  ASSERT_TRUE(nprocs == 1 || nprocs == 4);
+  if (nprocs > 1) distributed = true;
+#else
+  Wonton::SerialExecutor_type serial_executor;
+  executor = &serial_executor;
+#endif
+
+  const int D = 2;
+
+  // Create a 4x4 mesh
+  const std::vector<double> axis_points1 = {0.0, 1.0, 2.0, 3.0, 4.0};
+  std::array<std::vector<double>,D> axis_points;
+  for (int d = 0; d < D; ++d)
+    axis_points[d] = axis_points1;
+
+  // cell size in any direction
+  double delta = 1.0;
+
+  // This will create two owned cells per rank
+  Wonton::Direct_Product_Mesh<D> mesh(axis_points, executor);
+
+  // Dimensionality
+  ASSERT_EQ(mesh.space_dimension(), D);
+
+  // Point counts along each axis (since points shared by processors
+  // are owned on some and ghost on others, we cannot rely on separate
+  // owned, ghost point counts)
+
+  int NPOINTS_ALL = distributed ? 5 : 5;  // in dist. case (3 + 2)
+  
+  // Default query - ALL points
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NPOINTS_ALL, mesh.axis_num_points(d));
+
+  // ALL points queried explicitly
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NPOINTS_ALL, mesh.axis_num_points(d, ALL));
+
+  
+  // Cell counts along each axis
+  int NCELLS_OWNED = distributed ? 2 : 4;
+  int NCELLS_GHOST = distributed ? 2 : 0;
+  int NCELLS_ALL = NCELLS_OWNED + NCELLS_GHOST;
+  
+  // Default query - ALL cells
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELLS_ALL, mesh.axis_num_cells(d));
+
+  // All cells queried explicitly
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELLS_ALL, mesh.axis_num_cells(d, ALL));
+
+  // Owned cell counts
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELLS_OWNED, mesh.axis_num_cells(d, PARALLEL_OWNED));
+
+  // Ghost cell counts
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELLS_GHOST, mesh.axis_num_cells(d, PARALLEL_GHOST));
+
+  // Verify we are correctly identifying exterior points
+  for (int d = 0; d < D; ++d) {
+    double lo, hi;
+    mesh.get_global_coord_bounds(d, &lo, &hi);
+
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n;
+      double coord = mesh.get_axis_point(d, id);
+      if (fabs(coord) == Inf || coord == lo || coord == hi)
+        ASSERT_TRUE(mesh.point_on_external_boundary(d, id));
+      else
+        ASSERT_FALSE(mesh.point_on_external_boundary(d, id));
+    }
+  }
+  
+  // Cell coordinates (owned coordinate index starts from 0, ghost from -1)
+  for (int d = 0; d < D; ++d) {
+    double plo, phi;
+    mesh.get_local_coord_bounds(d, &plo, &phi);
+
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n;
+      double expval1, expval2;
+
+      if (n == 0) {
+        expval1 = plo;
+        expval2 = plo;
+      } else if (n == npall-1) {
+        expval1 = phi;
+        expval2 = phi;
+      } else {
+        int global_id = mesh.point_global_index(d, id);
+        int npaxis = axis_points[d].size();  // num input points along d
+        expval1 = (plo != -Inf) ? plo + delta*n :
+            axis_points[d][0] + delta*global_id;
+        expval2 = (phi !=  Inf) ? phi - delta*(npall-1-n) :
+            axis_points[d][npaxis-1] - delta*(npaxis-1-global_id);
+      }
+      
+      double coord = mesh.get_axis_point(d, id);
+      ASSERT_EQ(expval1, coord);
+      ASSERT_EQ(expval2, coord);
+    }
+  }
+}
+
+
+TEST(Direct_Product_Mesh, Parallel3D) {
+  double Inf = std::numeric_limits<double>::infinity();
+
+  bool distributed = false;
+  
+  Wonton::Executor_type *executor;
+#ifdef WONTON_ENABLE_MPI
+  Wonton::MPIExecutor_type parallel_executor(MPI_COMM_WORLD);
+  executor = &parallel_executor;
+
+  // Parallel checks will fail if we are not running this on 4 ranks
+  int nprocs;
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  ASSERT_TRUE(nprocs == 1 || nprocs == 4);
+  if (nprocs > 1) distributed = true;
+#else
+  Wonton::SerialExecutor_type serial_executor;
+  executor = &serial_executor;
+#endif
+
+  const int D = 3;
+
+  // Create a 4x4x2 mesh
+  const std::vector<double> axis_points01 = {0.0, 1.0, 2.0, 3.0, 4.0};
+  std::array<std::vector<double>,D> axis_points;
+  for (int d = 0; d < 2; ++d)
+    axis_points[d] = axis_points01;
+  axis_points[2] = std::vector<double>({-1.0, 0.0, 1.0});
+
+  // cell size in any direction
+  double delta = 1.0;
+
+  // Partition along two directions only (if distributed). This will
+  // create 8 owned cells per rank (2x2x2 blocks) per rank.
+  int dpart = distributed ? 2 : 0;
+  Wonton::Direct_Product_Mesh<D> mesh(axis_points, executor, dpart);
+
+  // Dimensionality
+  ASSERT_EQ(mesh.space_dimension(), D);
+
+  // Point counts along each axis (since points shared by processors
+  // are owned on some and ghost on others, we cannot rely on separate
+  // owned, ghost point counts)
+
+  // Default query - ALL points
+  for (int d = 0; d < D; ++d) {
+    int NPOINTS_ALL = 5;  // 5 in serial case; 3 owned + 2 ghost in dist case
+    if (d == 2 && !distributed)
+      NPOINTS_ALL = 3;          // only 3 points and no ghost points
+
+    ASSERT_EQ(NPOINTS_ALL, mesh.axis_num_points(d));
+  }
+
+  // ALL points queried explicitly
+  for (int d = 0; d < D; ++d) {
+    int NPOINTS_ALL = 5;  // 5 in serial case; 3 owned + 2 ghost in dist case
+    if (d == 2 && !distributed)
+      NPOINTS_ALL = 3;          // only 3 points and no ghost points
+    
+    ASSERT_EQ(NPOINTS_ALL, mesh.axis_num_points(d, ALL));
+  }
+
+  
+  // Cell counts along each axis
+  // Default query - ALL cells
+  for (int d = 0; d < D; ++d) {
+    int NCELLS_ALL = 4;  // 4 in serial case; 2 owned + 2 ghost in dist case
+    if (d == 2 && !distributed)
+      NCELLS_ALL = 2;
+    
+    ASSERT_EQ(NCELLS_ALL, mesh.axis_num_cells(d));
+  }
+
+  // All cells queried explicitly
+  for (int d = 0; d < D; ++d) {
+    int NCELLS_ALL = 4;  // 4 in serial case; 2 owned + 2 ghost in dist case
+    if (d == 2 && !distributed)
+      NCELLS_ALL = 2;
+    
+    ASSERT_EQ(NCELLS_ALL, mesh.axis_num_cells(d, ALL));
+  }
+
+  // Owned cell counts
+  for (int d = 0; d < D; ++d) {
+    int NCELLS_OWNED = distributed ? 2 : 4;
+    if (d == 2)
+      NCELLS_OWNED = 2;
+    ASSERT_EQ(NCELLS_OWNED, mesh.axis_num_cells(d, PARALLEL_OWNED));
+  }
+
+  // Ghost cell counts
+  int NCELLS_GHOST = distributed ? 2 : 0;
+  for (int d = 0; d < D; ++d)
+    ASSERT_EQ(NCELLS_GHOST, mesh.axis_num_cells(d, PARALLEL_GHOST));
+
+  // Verify we are correctly identifying exterior points
+  for (int d = 0; d < D; ++d) {
+    double lo, hi;
+    mesh.get_global_coord_bounds(d, &lo, &hi);
+
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n;
+      double coord = mesh.get_axis_point(d, id);
+      if (fabs(coord) == Inf || coord == lo || coord == hi)
+        ASSERT_TRUE(mesh.point_on_external_boundary(d, id));
+      else
+        ASSERT_FALSE(mesh.point_on_external_boundary(d, id));
+    }
+  }
+
+  
+  // Cell coordinates (owned coordinate index starts from 0, ghost from -1)
+  for (int d = 0; d < D; ++d) {
+    double plo, phi;
+    mesh.get_local_coord_bounds(d, &plo, &phi);
+
+    int npall = mesh.axis_num_points(d, ALL);
+    for (int n = 0; n < npall; ++n) {
+      int id = distributed ? n-1 : n;
+      double expval1, expval2;
+
+      if (n == 0) {
+        expval1 = plo;
+        expval2 = plo;
+      } else if (n == npall-1) {
+        expval1 = phi;
+        expval2 = phi;
+      } else {
+        int global_id = mesh.point_global_index(d, id);
+        int npaxis = axis_points[d].size();  // num input points along d
+        expval1 = (plo != -Inf) ? plo + delta*n :
+            axis_points[d][0] + delta*global_id;
+        expval2 = (phi !=  Inf) ? phi - delta*(npall-1-n) :
+            axis_points[d][npaxis-1] - delta*(npaxis-1-global_id);
+      }
+
+      double coord = mesh.get_axis_point(d, id);
+      ASSERT_EQ(expval1, coord);
+      ASSERT_EQ(expval2, coord);
+    }
+  }
+}
+
+
+#endif  // WONTON_ENABLE_MPI
--- wonton/mesh/direct_product/test/test_direct_product_mesh_wrapper.cc
+++ wonton/mesh/direct_product/test/test_direct_product_mesh_wrapper.cc
@@ -35,11 +35,15 @@ namespace direct_product_mesh_wrapper_test {
     for (int d = 0; d < D; ++d) {
       cell_count *= mesh_wrapper.axis_num_cells(d);
     }
-    ASSERT_EQ(mesh_wrapper.num_owned_cells(), cell_count);
+    ASSERT_TRUE(mesh_wrapper.num_owned_cells() >= 0);
+    ASSERT_TRUE(mesh_wrapper.num_ghost_cells() >= 0);
+    ASSERT_EQ(cell_count, mesh_wrapper.num_owned_cells()+mesh_wrapper.num_ghost_cells());
 
-    // For each axis
+    // For each axis check the points
     for (int d = 0; d < D; ++d) {
       ASSERT_EQ(mesh_wrapper.axis_num_cells(d), (int) axis_points[d].size()-1);
+
+      // We have to ignore the first and last point as they are ghosts
       int n = 0;
       for (auto iter = mesh_wrapper.axis_point_begin(d);
            iter != mesh_wrapper.axis_point_end(d);
@@ -64,9 +68,19 @@ namespace direct_product_mesh_wrapper_test {
     Wonton::Point<D> plo, phi;
     mesh_wrapper.cell_get_bounds(id, &plo, &phi);
     // Verify the bounding box
-    for (int d = 0; d < D; ++d) {
-      ASSERT_EQ(plo[d], axis_points[d][indices[d]]);
-      ASSERT_EQ(phi[d], axis_points[d][indices[d]+1]);
+    if (mesh_wrapper.distributed()) {
+      // axis_points[0] is the ghost point (index = -1) while
+      // axis_points[1] is the first owned point (index = 0)
+      for (int d = 0; d < D; ++d) {
+        ASSERT_EQ(plo[d], axis_points[d][indices[d]+1]);
+        ASSERT_EQ(phi[d], axis_points[d][indices[d]+2]);
+      }
+    } else {  // serial case
+      // axis_points[0] is first owned point (index = 0);
+      for (int d = 0; d < D; ++d) {
+        ASSERT_EQ(plo[d], axis_points[d][indices[d]]);
+        ASSERT_EQ(phi[d], axis_points[d][indices[d]+1]);
+      }
     }
   }
 
@@ -103,9 +117,19 @@ namespace direct_product_mesh_wrapper_test {
       const Wonton::Direct_Product_Mesh_Wrapper<D> &mesh_wrapper,
       const std::array<std::vector<double>,D> &axis_points) {
     if (d >= 0) {  // Recursive case
-      auto num_cells = mesh_wrapper.axis_num_cells(d);
-      for (indices[d] = 0; indices[d] < num_cells; ++indices[d]) {
-        loop_over_grid<D>(d-1, indices, id, mesh_wrapper, axis_points);
+      auto num_cells_all = mesh_wrapper.axis_num_cells(d);
+      if (mesh_wrapper.distributed()) {
+        int start_index = -1;
+        for (indices[d] = start_index; indices[d] < num_cells_all-1;
+             ++indices[d]) {
+          loop_over_grid<D>(d-1, indices, id, mesh_wrapper, axis_points);
+        }
+      } else {
+        int start_index = 0;
+        for (indices[d] = start_index; indices[d] < num_cells_all;
+             ++indices[d]) {
+          loop_over_grid<D>(d-1, indices, id, mesh_wrapper, axis_points);
+        }
       }
     } else {  // Base case
       // Verify cell bounding boxes
@@ -127,31 +151,33 @@ namespace direct_product_mesh_wrapper_test {
   void verify_cell_iteration(
       Wonton::Direct_Product_Mesh_Wrapper<D>& wrapper) {
     // Declare an array of zeros for storage
-    std::vector<int> found(wrapper.num_owned_cells(), 0);
+    int num_cells_all = wrapper.num_owned_cells()+wrapper.num_ghost_cells();
+    std::vector<int> found(num_cells_all, 0);
     // Loop over all cell IDs
     auto ekind = Wonton::Entity_kind::CELL;
-    auto etype = Wonton::Entity_type::PARALLEL_OWNED;
+    auto etype = Wonton::Entity_type::ALL;
     for (auto iter = wrapper.begin(ekind,etype);
         iter != wrapper.end(ekind,etype); ++iter) {
       // Get the indices from the cell ID
       auto id = (*iter);
       auto indices = wrapper.cellid_to_indices(id);
       // Convert the indices to an arbitrary 1D index for storage array
-      std::size_t n = indices[0];
-      for (int d = 1; d < D; ++d) {
+      std::size_t n = 0;
+      for (int d = D-1; d > 0; --d) {
+        n += (wrapper.distributed() ? indices[d]+1 : indices[d]);
         n *= wrapper.axis_num_cells(d-1);
-        n += indices[d];
       }
+      n += wrapper.distributed() ? indices[0]+1 : indices[0];
       // Ensure that the 1D index is in the valid range
       ASSERT_GE(n, 0);
-      ASSERT_LT(n, wrapper.num_owned_cells());
+      ASSERT_LT(n, num_cells_all);
       // Mark that value as having been found
       found[n] += 1;
     }
     // Verify that all elements were found
     // -- Did we find the right number?
     auto num_found = std::accumulate(found.begin(), found.end(), 0);
-    ASSERT_EQ(num_found, wrapper.num_owned_cells());
+    ASSERT_EQ(num_found, num_cells_all);
     // -- Was every element found?
     int minimum = *std::min_element(found.begin(), found.end());
     ASSERT_EQ(minimum, 1);
@@ -163,20 +189,47 @@ namespace direct_product_mesh_wrapper_test {
   // --------------------------------------------------------------------------
 
   template<int D>
-  void run_all_tests(const std::vector<double> &axis_points1) {
+  void run_all_tests(const std::vector<double> &axis_points_in) {
     // Build mesh and wrapper
-    std::array<std::vector<double>,D> axis_points;
+    std::array<std::vector<double>,D> axis_points_global;
     for (int d = 0; d < D; ++d) {
-      axis_points[d] = axis_points1;
+      axis_points_global[d] = axis_points_in;
     }
-    Wonton::Direct_Product_Mesh<D> mesh(axis_points);
+
+
+    Wonton::Executor_type *executor;
+#ifdef WONTON_ENABLE_MPI
+    Wonton::MPIExecutor_type parallel_exec(MPI_COMM_WORLD);
+    executor = &parallel_exec;
+#else
+    Wonton::SerialExecutor_type serial_exec;
+    executor = &serial_exec;
+#endif
+    
+    Wonton::Direct_Product_Mesh<D> mesh(axis_points_global, executor);
     Wonton::Direct_Product_Mesh_Wrapper<D> mesh_wrapper(mesh);
 
+    // collect the points on this partition (all points bounding OWNED
+    // cells, which means all points except the OUTER two in any
+    // direction)
+    std::array<std::vector<double>,D> axis_points;
+    for (int d = 0; d < D; d++) {
+      int n = mesh.axis_num_points(d, Wonton::ALL);
+      axis_points[d].resize(n);
+      if (mesh.distributed())
+        for (int i = -1; i < n-1; i++)
+          axis_points[d][i+1] = mesh.get_axis_point(d, i);
+      else
+        for (int i = 0; i < n; i++)
+          axis_points[d][i] = mesh.get_axis_point(d, i);
+    }
+    
     // Run basic tests
     direct_product_mesh_wrapper_test::check_basic_functions(
         mesh_wrapper, axis_points);
 
     // Run looping tests
+
     int id = 0;
     std::array<int,D> indices;
     direct_product_mesh_wrapper_test::loop_over_grid<D>(
@@ -191,10 +244,10 @@ namespace direct_product_mesh_wrapper_test {
 
 // ============================================================================
 
-TEST(Direct_Product_Mesh_Wrapper, SmallGrid1D) {
-  const std::vector<double> axis_points1 = {0.0625, 0.125, 0.25, 0.5, 1.0};
-  direct_product_mesh_wrapper_test::run_all_tests<1>(axis_points1);
-}
+// TEST(Direct_Product_Mesh_Wrapper, SmallGrid1D) {
+//   const std::vector<double> axis_points1 = {0.0625, 0.125, 0.25, 0.5, 1.0};
+//   direct_product_mesh_wrapper_test::run_all_tests<1>(axis_points1);
+// }
 
 
 // ============================================================================
@@ -215,7 +268,7 @@ TEST(Direct_Product_Mesh_Wrapper, OneCell3D) {
 
 // ============================================================================
 
-TEST(Direct_Product_Mesh_Wrapper, SmalGrid10D) {
+TEST(Direct_Product_Mesh_Wrapper, SmallGrid10D) {
   const std::vector<double> axis_points1 = {0.0, 0.5, 1.0};
   direct_product_mesh_wrapper_test::run_all_tests<10>(axis_points1);
 }
