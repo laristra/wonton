03d74371ae34418ec9015ad63407b37ffa3ec3a8
Rao Garimella <rao@lanl.gov>
2020-02-12 13:48:09 -0700

    add tests for cell-cell and node-node adjacency retrieval operators

--- wonton/mesh/direct_product/test/test_direct_product_mesh_wrapper.cc
+++ wonton/mesh/direct_product/test/test_direct_product_mesh_wrapper.cc
@@ -201,6 +201,90 @@ namespace direct_product_mesh_wrapper_test {
   // --------------------------------------------------------------------------
 
   template<int D>
+  void verify_cell_adjacencies(
+      Wonton::Direct_Product_Mesh_Wrapper<D>& wrapper) {    
+    // Loop over all cell IDs
+    Wonton::Entity_kind ekind = Wonton::CELL;
+    Wonton::Entity_type etype = Wonton::ALL;
+    for (auto iter = wrapper.begin(ekind,etype);
+        iter != wrapper.end(ekind,etype); ++iter) {
+      // Get the indices from the cell ID
+      auto id = (*iter);
+      auto indices = wrapper.cellid_to_indices(id);
+
+      std::vector<int> adjcells;
+      wrapper.cell_get_node_adj_cells(id, etype, &adjcells);
+
+      // Make sure we got the right number?
+      int num_ghost_layers = wrapper.mesh().num_ghost_layers();
+      std::array<int, D> nindices;
+      for (int d = 0; d < D; d++) {
+        int num_points_all = wrapper.mesh().axis_num_points(d, Wonton::ALL);
+        int lo = (indices[d]-1 >= -num_ghost_layers) ?
+            indices[d]-1 : indices[d];
+        int hi = (indices[d]+1 < num_points_all-num_ghost_layers-1) ?
+            indices[d]+1 : indices[d];
+        nindices[d] = hi-lo+1;
+      }
+      int expnum = 1;
+      for (int d = 0; d < D; d++)
+        expnum *= nindices[d];
+      ASSERT_EQ(expnum, adjcells.size());
+      
+      // Make sure the indices of the cells are no more than one off
+      for (auto adjid : adjcells) {
+        auto adjindices = wrapper.cellid_to_indices(adjid);
+        for (int d = 0; d < D; d++)
+          ASSERT_LE(fabs(indices[d]-adjindices[d]), 1);
+      }
+    }
+  }
+
+  // --------------------------------------------------------------------------
+
+  template<int D>
+  void verify_node_adjacencies(
+      Wonton::Direct_Product_Mesh_Wrapper<D>& wrapper) {    
+    // Loop over all node IDs
+    Wonton::Entity_kind ekind = Wonton::NODE;
+    Wonton::Entity_type etype = Wonton::ALL;
+    for (auto iter = wrapper.begin(ekind,etype);
+        iter != wrapper.end(ekind,etype); ++iter) {
+      // Get the indices from the node ID
+      auto id = (*iter);
+      auto indices = wrapper.nodeid_to_indices(id);
+
+      std::vector<int> adjnodes;
+      wrapper.node_get_cell_adj_nodes(id, etype, &adjnodes);
+
+      // Make sure we got the right number?
+      int num_ghost_layers = wrapper.mesh().num_ghost_layers();
+      std::array<int, D> nindices;
+      for (int d = 0; d < D; d++) {
+        int num_points_all = wrapper.mesh().axis_num_points(d, Wonton::ALL);
+        int lo = (indices[d]-1 >= -num_ghost_layers) ?
+            indices[d]-1 : indices[d];
+        int hi = (indices[d]+1 < num_points_all-num_ghost_layers) ?
+            indices[d]+1 : indices[d];
+        nindices[d] = hi-lo+1;
+      }
+      int expnum = 1;
+      for (int d = 0; d < D; d++)
+        expnum *= nindices[d];
+      ASSERT_EQ(expnum, adjnodes.size());
+      
+      // Make sure the indices of the cells are no more than one off
+      for (auto adjid : adjnodes) {
+        auto adjindices = wrapper.nodeid_to_indices(adjid);
+        for (int d = 0; d < D; d++)
+          ASSERT_LE(fabs(indices[d]-adjindices[d]), 1);
+      }
+    }
+  }
+
+  // --------------------------------------------------------------------------
+
+  template<int D>
   void run_all_tests(const std::vector<double> &axis_points_in) {
     // Build mesh and wrapper
     std::array<std::vector<double>,D> axis_points_global;
@@ -211,7 +295,7 @@ namespace direct_product_mesh_wrapper_test {
     Wonton::Executor_type *executor;
     bool distributed = false;
 #ifdef WONTON_ENABLE_MPI
-    if (D > 3) return;  // cannot partition in anything grater than 3D
+    if (D > 3) return;  // cannot partition in anything greater than 3D
     Wonton::MPIExecutor_type parallel_exec(MPI_COMM_WORLD);
     executor = &parallel_exec;
     int nprocs;
@@ -251,6 +335,10 @@ namespace direct_product_mesh_wrapper_test {
 
       // Verify cell iteration
       verify_cell_iteration(mesh_wrapper);
+
+      // Check adjacencies
+      verify_cell_adjacencies<D>(mesh_wrapper);
+      verify_node_adjacencies<D>(mesh_wrapper);
     }
   }
 
