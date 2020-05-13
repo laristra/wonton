711dca597a86e64085914dc882ae01511d37d10e
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-05-13 14:34:30 -0600

    Fix error-prone initializations

--- wonton/support/test/test_structured_partitioner.cc
+++ wonton/support/test/test_structured_partitioner.cc
@@ -5,6 +5,7 @@ Please see the license file at the root of this repository, or at:
 */
 
 #include <iostream>
+#include <array>
 
 #include "gtest/gtest.h"
 #include "wonton/support/structured_partitioner.h"
@@ -16,9 +17,13 @@ TEST(StructPartitioner, Serial) {
     std::array<int64_t, 2> ncells = {2, 2};
     auto partlimits = Wonton::structured_partitioner<2>(4, ncells);
 
-    std::array<std::array<std::array<int64_t, 2>, 2>, 4> exp_partlimits =
-        {0,0, 1,1,  0,1, 1,2,   1,0, 2,1,  1,1, 2,2};
-    
+    std::vector<std::vector<std::array<int64_t, 2>>> exp_partlimits = {
+      {{0, 0}, {1, 1}},
+      {{0, 1}, {1, 2}},
+      {{1, 0}, {2, 1}},
+      {{1, 1}, {2, 2}}
+    };
+
     for (int i = 0; i < 4; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
@@ -30,9 +35,13 @@ TEST(StructPartitioner, Serial) {
 
     auto partlimits = Wonton::structured_partitioner<2>(4, ncells);
 
-    std::array<std::array<std::array<int64_t, 2>, 2>, 4> exp_partlimits =
-        {0,0, 1,2,  0,2, 1,3,  1,0, 2,2,  1,2, 2,3};
-    
+    std::vector<std::vector<std::array<int64_t, 2>>> exp_partlimits = {
+      {{0, 0}, {1, 2}},
+      {{0, 2}, {1, 3}},
+      {{1, 0}, {2, 2}},
+      {{1, 2}, {2, 3}}
+    };
+
     for (int i = 0; i < 4; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
@@ -44,23 +53,30 @@ TEST(StructPartitioner, Serial) {
 
     auto partlimits = Wonton::structured_partitioner<2>(3, ncells);
 
-    std::array<std::array<std::array<int64_t, 2>, 2>, 3> exp_partlimits =
-        { 0,0, 19,10,   19,0, 37,10,   37,0, 55,10 };  // Removing the outermost braces causes compilation error
-    
+    std::vector<std::vector<std::array<int64_t, 2>>> exp_partlimits = {
+      {{ 0, 0}, {19, 10}},
+      {{19, 0}, {37, 10}},
+      {{37, 0}, {55, 10}}
+    };
+
     for (int i = 0; i < 3; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
   }
-  
+
   {
     // Partition 55x10 mesh into 4 partitions BUT ONLY IN 1 DIR
     std::array<int64_t, 2> ncells = {55, 10};
 
     auto partlimits = Wonton::structured_partitioner<2>(4, ncells, 1);
 
-    std::array<std::array<std::array<int64_t, 2>, 2>, 4> exp_partlimits =
-        { 0,0, 14,10,  14,0, 28,10,  28,0, 42,10,  42,0, 55,10 };
-    
+    std::vector<std::vector<std::array<int64_t, 2>>> exp_partlimits = {
+      {{ 0, 0}, {14, 10}},
+      {{14, 0}, {28, 10}},
+      {{28, 0}, {42, 10}},
+      {{42, 0}, {55, 10}}
+    };
+
     for (int i = 0; i < 4; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
@@ -73,13 +89,18 @@ TEST(StructPartitioner, Serial) {
 
     auto partlimits = Wonton::structured_partitioner<3>(8, ncells);
 
-    std::array<std::array<std::array<int64_t, 3>, 2>, 8> exp_partlimits =
-        { 0,0,0,  28,5,6,     0,0,6, 28,5,12,
-          0,5,0,  28,10,6,    0,5,6, 28,10,12,
-          28,0,0, 55,5,6,    28,0,6, 55,5,12,
-          28,5,0, 55,10,6,   28,5,6, 55,10,12 };
-           
-    
+    std::vector<std::vector<std::array<int64_t, 3>>> exp_partlimits = {
+      {{ 0, 0, 0}, {28,  5,  6}},
+      {{ 0, 0, 6}, {28,  5, 12}},
+      {{ 0, 5, 0}, {28, 10,  6}},
+      {{ 0, 5, 6}, {28, 10, 12}},
+      {{28, 0, 0}, {55,  5,  6}},
+      {{28, 0, 6}, {55,  5, 12}},
+      {{28, 5, 0}, {55, 10,  6}},
+      {{28, 5, 6}, {55, 10, 12}}
+    };
+
+
     for (int i = 0; i < 8; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
@@ -92,13 +113,18 @@ TEST(StructPartitioner, Serial) {
 
     auto partlimits = Wonton::structured_partitioner<3>(8, ncells, 2);
 
-    std::array<std::array<std::array<int64_t, 3>, 2>, 8> exp_partlimits =
-        { 0,0,0, 14,5,12,    0,5,0, 14,10,12,
-         14,0,0, 28,5,12,   14,5,0, 28,10,12,
-         28,0,0, 42,5,12,   28,5,0, 42,10,12,
-         42,0,0, 55,5,12,   42,5,0, 55,10,12 };
-           
-    
+    std::vector<std::vector<std::array<int64_t, 3>>> exp_partlimits = {
+      {{ 0, 0, 0}, {14,  5, 12}},
+      {{ 0, 5, 0}, {14, 10, 12}},
+      {{14, 0, 0}, {28,  5, 12}},
+      {{14, 5, 0}, {28, 10, 12}},
+      {{28, 0, 0}, {42,  5, 12}},
+      {{28, 5, 0}, {42, 10, 12}},
+      {{42, 0, 0}, {55,  5, 12}},
+      {{42, 5, 0}, {55, 10, 12}}
+    };
+
+
     for (int i = 0; i < 8; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
@@ -111,10 +137,14 @@ TEST(StructPartitioner, Serial) {
 
     auto partlimits = Wonton::structured_partitioner<3>(5, ncells, 1);
 
-    std::array<std::array<std::array<int64_t, 3>, 2>, 5> exp_partlimits =
-        { 0,0,0, 11,10,5,   11,0,0, 22,10,5,  22,0,0, 33,10,5,
-         33,0,0, 44,10,5,   44,0,0, 55,10,5 };
-    
+    std::vector<std::vector<std::array<int64_t, 3>>> exp_partlimits = {
+      {{ 0, 0, 0}, {11, 10, 5}},
+      {{11, 0, 0}, {22, 10, 5}},
+      {{22, 0, 0}, {33, 10, 5}},
+      {{33, 0, 0}, {44, 10, 5}},
+      {{44, 0, 0}, {55, 10, 5}}
+    };
+
     for (int i = 0; i < 5; i++)
       for (int j = 0; j < 2; j++)
         ASSERT_EQ(exp_partlimits[i][j], partlimits[i][j]);
